1f6460f72f7f1121b37ead2a2a9b7a23a0c998bc
orlando-mercado <orlando.mercado1@upr.edu>
2021-03-23 11:10:10 -0400

    Added comments

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+//Simple Calculator app
 public class Calculator implements ActionListener {
 	JFrame frame;
 	JTextField textfield;
